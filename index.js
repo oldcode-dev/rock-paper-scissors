69e6f8b2b7b53e265c07921d7d11c95df7c1c795
Ernest Frimpong Opoku <ernestfrimpongopoku.992@outlook.com>
2025-07-21 03:50:09 +0000

    Add playRound to simulate game

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-function getComputerChoice(){
+function getComputerChoice() {
     const decision = ["rock", "paper", "scissors"];
 
     const choice = decision[Math.floor(Math.random() * 3)];
     return choice;
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let decision = prompt('Enter your decision').toLowerCase();
     return decision;
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return "It's a tie!";
     }
@@ -31,6 +31,22 @@ function playRound(humanChoice, computerChoice){
         return `You lose! ${computerChoice} beats ${humanChoice}.`;
     }
 }
+
+function playGame() {
+    let round = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        const result = playRound(humanChoice, computerChoice);
+        console.log(result);
+        console.log(`Round ${round + 1} Scores: computer ${computerScore} - human ${humanScore}`);
+        round++;
+    }
+}
+
+playGame();
 
 
 
